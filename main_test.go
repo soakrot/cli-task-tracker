b5ffb4ee76e028a5edd8d9edc7082073b3943e6d
intellectualen <omaryasser_250@hotmail.com>
2025-12-02 15:08:48 +0200

    test: add test for deleting tasks"

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -58,6 +59,34 @@ func TestUpdateTask(t *testing.T) {
 
 			if task != tt.want {
 				t.Errorf("got %s, want %s", task, tt.want)
+			}
+		})
+	}
+}
+
+func TestDeleteTask(t *testing.T) {
+	store := Store{Tasks: make(map[uint]*Task), NextID: 1}
+	store.AddTask("Task 1")
+	store.AddTask("Task 2")
+	store.AddTask("Task 3")
+
+	tests := []struct {
+		id   uint
+		want string
+	}{
+		{1, "task 1"},
+		{2, "task 2"},
+		{3, "task 3"},
+		{4, ""},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s", tt.want)
+		t.Run(testname, func(t *testing.T) {
+			out, _ := store.DeleteTask(tt.id)
+
+			if strings.ToLower(out) != tt.want {
+				t.Errorf("got %q, want %q", out, tt.want)
 			}
 		})
 	}
