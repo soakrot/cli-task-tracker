d383ed8e40b76e7ed25485aa6643ecbf5a5a4732
intellectualen <omaryasser_250@hotmail.com>
2025-12-02 15:07:58 +0200

    test: add test for updating tasks

--- main_test.go
+++ main_test.go
@@ -30,3 +30,35 @@ func TestAddTask(t *testing.T) {
 	}
 
 }
+
+func TestUpdateTask(t *testing.T) {
+	store := Store{Tasks: make(map[uint]*Task), NextID: 1}
+	store.AddTask("Task 1")
+	store.AddTask("Task 2")
+	store.AddTask("Task 3")
+
+	tests := []struct {
+		id   uint
+		c    string
+		want string
+	}{
+		{1, "something else", "something else"},
+		{2, "something else 2", "something else 2"},
+		{3, "ummm", "ummm"},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s", tt.c)
+		t.Run(testname, func(t *testing.T) {
+			err := store.UpdateTask(tt.id, tt.c)
+			task := store.Tasks[tt.id].Description
+			if err != nil {
+				t.Errorf("error: %s", err)
+			}
+
+			if task != tt.want {
+				t.Errorf("got %s, want %s", task, tt.want)
+			}
+		})
+	}
+}
